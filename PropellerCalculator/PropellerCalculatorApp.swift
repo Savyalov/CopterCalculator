1b7e35f67142579486091e8f3dd327cd5ffc0272
Konstantin Savyalov <ksavyalov@gmail.com>
2025-11-14 02:35:55 +0300

    Initial Commit

---
+++ PropellerCalculator/PropellerCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PropellerCalculatorApp.swift
+//  PropellerCalculator
+//
+//  Created by Константин Савялов on 14.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct PropellerCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
